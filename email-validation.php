d96e7d5d6cc4064dbb6c5218b6059f17f58668fe
Ieva Šinke <ieva.sinke@gmail.com>
2024-05-20 10:11:20 +0300

    emailvalidation.io API, sending email via mailgun

--- email-validation.php
+++ email-validation.php
@@ -1,3 +1,36 @@
 <?php
+/*
+ * Use https://emailvalidation.io/ API to create an application that
+ * allows to enter the email address and return if its valid or not.
+ *
+ * Send an email with basic "hello world" information to validated email
+ * You can use anything from different APis, mailgun, sparkpost, gmail, smtp services etc.
+ * TODO export API_KEY_EMAIL=yourownkey
+ * TODO export API_KEY_MAILGUN=yourownkey
+ */
 
-$appid = getenv("API_KEY_EMAIL");
+require 'vendor/autoload.php';
+use Mailgun\Mailgun;
+
+$appKeyValidation = getenv("API_KEY_EMAIL");
+$emailToValidate = (string)readline("Enter the email address for validation: ");
+$data = file_get_contents("https://api.emailvalidation.io/v1/info?apikey=$appKeyValidation&email=$emailToValidate");
+$emailsData = json_decode($data);
+$apiKeyMailgun = getenv("API_KEY_MAILGUN");
+$domain = "sandbox139eb76be3f444ad90f13165a0df1cf0.mailgun.org";
+echo "Email validity score: $emailsData->score\n";
+
+if ($emailsData->format_valid) {
+    try {
+        $mg = Mailgun::create($apiKeyMailgun);
+
+        $result = $mg->messages()->send($domain, [
+            'from' => 'Mailgun Sandbox <postmaster@sandbox139eb76be3f444ad90f13165a0df1cf0.mailgun.org>',
+            'to' => $emailToValidate,
+            'subject' => 'The PHP is awesome!',
+            'text' => 'hello world'
+        ]);
+    } catch (\Mailgun\Exception\HttpClientException $e) {
+        echo "Mailgun API Exception: " . $e->getMessage();
+    }
+}
